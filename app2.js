7574b996da1af1c63534beaa8958a4a851097f37
Jeff Blankenship <jsblankenship44@gmail.com>
2017-04-07 13:42:45 -0400

    Add files via upload

---
+++ app2.js
@@ -0,0 +1,22 @@
+// JavaScript Document
+
+var totalf = 0, count = 1;
+while (count <= 1000) {
+    totalf += count;
+    count += 1;
+}
+
+var totalw = 0, count = 1;
+while (count <= 1000) {
+    totalw += count;
+    count += 1;
+}
+
+console.log(totalw);	
+console.log(totalf);
+
+
+
+
+
+
